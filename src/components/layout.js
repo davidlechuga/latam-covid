015cda1b4c51c52b1263b27bd6a0cd6db76d8776
kerd947 <d_kempke94@hotmail.com>
2020-04-12 20:53:34 -0500

    fixed cofee layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,22 @@ import Footer from "../components/Footer";
 import "../style/layout.css";
 import LocalCafeIcon from '@material-ui/icons/LocalCafe';
 import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	fab: {
+	  margin: theme.spacing.unit,
+	  position: "fixed",
+	  bottom: theme.spacing.unit * 2,
+	  right: theme.spacing.unit * 3,
+	  zIndex:9999
+	},
+  }));
 
 export default ({ elevateAppBar = true, children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const handleToggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
+  const classes = useStyles();
 
   return (
     <ThemeProvider theme={theme}>
@@ -20,20 +32,11 @@ export default ({ elevateAppBar = true, children }) => {
         onToggleDrawer={handleToggleDrawer}
         elevation={Number(elevateAppBar)}
       />
-	  <Fab size="medium" color="secondary" aria-label="add" href="https://ko-fi.com/coronantine">
+	  
+      {children}
+	  <Fab size="medium" color="secondary" aria-label="add" href="https://ko-fi.com/coronantine" className={classes.fab}>
           <LocalCafeIcon />
 	  </Fab>
-      {children}
-	  <Fab
-          variant="extended"
-          size="medium"
-          color="primary"
-		  aria-label="add"
-		  href="https://ko-fi.com/coronantine"
-        >
-          <LocalCafeIcon/>
-          Buy us a coffee
-        </Fab>
       <Footer />
       <Drawer open={isDrawerOpen} onClose={handleToggleDrawer} />
     </ThemeProvider>
